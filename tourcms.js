c53f9f798e17852c9b429da0342207f3536cc7b5
palisis-frmar <francisco.martinez@palisis.com>
2024-01-18 16:23:47 +0100

    TOURCMS-9408 create get tour list endpoint functionality

--- tourcms.js
+++ tourcms.js
@@ -682,6 +682,26 @@ TourcmsApi = function (p) {
 		});
 	}
 
+	this.getListTours = function (a) {
+		// Sensible defaults
+		if (typeof a.params.channelId === "undefined")
+			a.params.channelId = 0;
+
+		if (typeof a.params.qs === "undefined")
+			a.params.qs = "";
+
+		// Set API path
+		path = '/api/tours/importer/get_tour_list.xml?'+a.params.qs;
+
+		// Call API
+		this.request({
+			"path": path,
+			"channelId": a.params.channelId,
+			"callback": a.callback,
+			"callbackError": a.callbackError
+		});
+	}
+
 	// Generate the signature required to sign API calls
 	this.generateSignature = function (path, channelId, verb, outboundTime) {
 		var stringToSign = channelId + "/" + this.marketplaceId + "/" + verb + "/" + outboundTime + path;
